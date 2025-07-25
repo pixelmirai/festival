0590fb95982a7b3da9a5b085da4e6aaff4a52b9e
pixelmirai <pixelmirai@gmail.com>
2025-07-25 13:59:15 +0200

    bottom slider

--- website/components/Section/Third.vue
+++ website/components/Section/Third.vue
@@ -4,7 +4,7 @@
 
 <template>
 
-      <div class="w-full bg-white">
+      <div class="w-screen  bg-white">
 
         <div class="w-full h-[20rem]">
           <div class="h-[20rem] max-w-screen-4xl overflow-x-hidden mx-auto">
@@ -40,39 +40,7 @@
           </div>
         </div>
 
-        <div class="w-full h-[20rem]">
-          <div class="h-[20rem] max-w-screen-4xl overflow-x-hidden mx-auto">
-            <div class="slider-2-movement flex  min-w-max h-full ">
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/maza-gilde-orchestra.webp" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/ex-silentio.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/regence-sonore-2.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem]  " style="aspect-ratio: 1.6">
-                <img src="assets/images/regence-sonore.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/regence-sonore-3.png" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/maza-gilde.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/maza-gilde-orchestra.webp" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/ex-silentio.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-              <div class="h-[20rem] " style="aspect-ratio: 1.6">
-                <img src="assets/images/regence-sonore-2.jpg" alt="" class="w-full h-full object-cover">
-              </div>
-            </div>
-          </div>
-        </div>
+
 
       </div>
 
@@ -80,7 +48,7 @@
 
 <style scoped>
 .slider-movement{
-  will-change: transform;
+
   animation: sm 15s linear infinite 1s;
 }
 
@@ -95,7 +63,7 @@
 
 }
 .slider-2-movement{
-  will-change: transform;
+
   transform: translateX(-20%);
   animation: sm-2 35s linear infinite 1s;
 }
