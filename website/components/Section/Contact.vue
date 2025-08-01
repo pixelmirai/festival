dd7e2a586bea96e3ce5767e8a30045f90a5e38fe
pixelmirai <pixelmirai@gmail.com>
2025-08-01 14:56:57 +0200

    contact layout

--- website/components/Section/Contact.vue
+++ website/components/Section/Contact.vue
@@ -12,15 +12,19 @@
 
     <div class="w-screen flex justify-center bg-[#1a1a1a]  ">
       <div class="card-maris-kupcs relative aspect-square bg-[#1a1a1a]  text-[#d4af37]  h-[20rem]   shadow-lg font-serif ">
-        <div class="absolute flex flex-col space-y-4   justify-center items-center w-full h-full   ">
-          <div class="  text-2xl text-center tracking-wide font-semibold uppercase">
+        <div class="absolute flex flex-col py-[3rem]   items-center w-full h-full   ">
+
+          <div class="flex   h-1/6   text-2xl text-center tracking-wide font-semibold uppercase">
             MĀRIS  KUPČS
           </div>
-          <div class="  tracking-widest text-center italic">
-            Mākslinieciskais vadītājs <br> & <br> galvenais diriğents
+          <div class="flex justify-center items-center h-1/2  tracking-widest text-center italic">
+            <span>
+                Mākslinieciskais vadītājs <br> & <br> galvenais diriğents
+            </span>
+
           </div>
 
-          <div class="  flex flex-col justify-center items-center ">
+          <div class=" h-1/3  flex flex-col justify-end  items-center ">
             <div class=" ">
               <i class="bi bi-envelope-fill text-[2rem]"></i>
             </div>
@@ -28,59 +32,80 @@
               <span>maris.kupcs@earlymusic.lv</span>
             </div>
           </div>
+
         </div>
+
+
       </div>
     </div>
 
     <div class="w-screen flex justify-center bg-[#0f1b2a]  ">
       <div class="card-aija-dimza relative aspect-square bg-[#0f1b2a] text-[#c9a66b] h-[20rem]   font-serif ">
-        <div class="absolute flex flex-col space-y-12     justify-center items-center w-full h-full  ">
-          <div class="flex flex-col space-y-4 ">
-            <div class="text-2xl text-center tracking-wide font-semibold uppercase">
-              Aija Dimza
+        <div class="absolute flex flex-col     justify-center items-center w-full h-full  ">
+
+          <div class="flex flex-col  py-[3rem] w-full h-full">
+
+
+              <div class="text-2xl h-1/6 text-center tracking-wide font-semibold uppercase">
+                <span>
+                   Aija Dimza
+                </span>
+              </div>
+              <div class="flex justify-center items-center   h-1/2 tracking-widest text-center italic">
+                <span>
+                   Vadītāja <br> &  <br> producente
+                </span>
+              </div>
+
+
+
+            <div class="flex justify-center space-x-8 h-1/3 w-full  ">
+              <div class=" flex flex-col w justify-center items-center w-1/3  ">
+                <div class="">
+                  <i class="bi bi-phone-fill  text-[2rem]"></i>
+                </div>
+                <div class="">
+                  <span>+371 22477541</span>
+                </div>
+              </div>
+              <div class="  flex flex-col justify-center items-center w-1/3     ">
+                <div class=" ">
+                  <i class="bi bi-envelope-fill text-[2rem]"></i>
+                </div>
+                <div class=" ">
+                  <span>info@earlymusic.lv</span>
+                </div>
+              </div>
             </div>
-            <div class=" tracking-widest text-center italic">
-              vadītāja & producente
-            </div>
+
           </div>
 
 
-          <div class="flex justify-center space-x-8 w-full  ">
-            <div class=" flex flex-col w justify-center items-center w-1/3  ">
-              <div class="">
-                <i class="bi bi-phone-fill  text-[2rem]"></i>
-              </div>
-              <div class="">
-                <span>+371 22477541</span>
-              </div>
-            </div>
-            <div class="  flex flex-col justify-center items-center w-1/3     ">
-              <div class=" ">
-                <i class="bi bi-envelope-fill text-[2rem]"></i>
-              </div>
-              <div class=" ">
-                <span>info@earlymusic.lv</span>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
 
-    <div class="flex flex-col  justify-center items-center h-[20rem] aspect-square text-white">
-      <div class="w-[8rem]   ">
-        <img src="assets/images/logo.webp" alt="" class="w-full h-full">
-      </div>
-      <div class="text-xl">
-        <span>SIA</span> <span class="italic font-serif "> Rīgas Senās mūzikas centrs</span>
-      </div>
-      <div>
-        <i class="bi bi-map"></i>
-      </div>
-      <div>
-        <span>Vaļņu iela 9-3, Rīga, LV-1050</span>
+    <div class="w-screen flex justify-center bg-black   ">
+      <div class="flex flex-col  justify-between  items-center py-[3rem] h-[20rem] aspect-square  text-white bg-black">
+        <div class="w-[8rem] h-1/3  ">
+          <img src="assets/images/logo.webp" alt="" class="w-full h-full">
+        </div>
+        <div class="flex justify-center items-center text-xl h-1/3   ">
+          <span class="font-extralight -translate-y-[1px] ">SIA </span> <span class="italic font-serif  ml-1 "> Rīgas Senās mūzikas centrs</span>
+        </div>
+        <div class="flex flex-col justify-center items-center h-1/3  ">
+          <div class="h-1/2  text-2xl  ">
+            <i class="bi bi-map"></i>
+          </div>
+          <div class="h-1/2  ">
+            <span>Vaļņu iela 9-3, Rīga, LV-1050</span>
+          </div>
+        </div>
+
       </div>
     </div>
+
+
 
   </div>
 
