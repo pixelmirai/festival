8f79a67d157a4db97914c665f1e0db9d65a67283
pixelmirai <pixelmirai@gmail.com>
2025-08-01 15:00:36 +0200

    gmap

--- website/components/Section/Contact.vue
+++ website/components/Section/Contact.vue
@@ -104,6 +104,9 @@
 
       </div>
     </div>
+    <div class="w-screen">
+      <iframe class="w-full h-[20rem]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2598.0651446728034!2d24.107608077161537!3d56.95025799912953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfd124d75647%3A0x184f72b13af16db9!2zVmHEvMWGdSBpZWxhIDksIENlbnRyYSByYWpvbnMsIFLEq2dhLCBMVi0xMDUwLCBMYXR2aWE!5e1!3m2!1sen!2sat!4v1754053100948!5m2!1sen!2sat" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 
 
 
