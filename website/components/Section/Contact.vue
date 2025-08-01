00a5330df839fb4ce3f9e7cdf958137ae122b7d3
pixelmirai <pixelmirai@gmail.com>
2025-08-01 16:56:00 +0200

    fb icon

--- website/components/Section/Contact.vue
+++ website/components/Section/Contact.vue
@@ -4,8 +4,8 @@
 
 <template>
 <div class="min-h-screen n  bg-black">
-  <div class="w-[20rem] h-[6rem]">
-
+  <div class="flex justify-center items-center w-[20rem] mx-auto   h-[6rem]">
+      <ContactSocialIcons/>
   </div>
 
   <div class="flex flex-col items-center">
@@ -105,7 +105,7 @@
       </div>
     </div>
     <div class="w-screen">
-      <iframe class="w-full h-[20rem]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2598.0651446728034!2d24.107608077161537!3d56.95025799912953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfd124d75647%3A0x184f72b13af16db9!2zVmHEvMWGdSBpZWxhIDksIENlbnRyYSByYWpvbnMsIFLEq2dhLCBMVi0xMDUwLCBMYXR2aWE!5e1!3m2!1sen!2sat!4v1754053100948!5m2!1sen!2sat" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe class="w-screen  h-[20rem]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2598.0651446728034!2d24.107608077161537!3d56.95025799912953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eecfd124d75647%3A0x184f72b13af16db9!2zVmHEvMWGdSBpZWxhIDksIENlbnRyYSByYWpvbnMsIFLEq2dhLCBMVi0xMDUwLCBMYXR2aWE!5e1!3m2!1sen!2sat!4v1754053100948!5m2!1sen!2sat" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
 
